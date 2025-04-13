8c52458a5c1034e63e020a889dcaf98b6ed4d6f4
Salim Dellali <dellali.salim@gmail.com>
2025-04-13 17:13:39 -0400

    revert prisma usage, not suitable for an app built with next.js with supabase deployed on vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,11 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "**/src/generated/**", // Ignore all files in generated folder
-    ],
-  },
 ]
 
 export default eslintConfig
--- src/actions/todos/get-all-todos.ts
+++ src/actions/todos/get-all-todos.ts
@@ -1,9 +1,9 @@
 "use server"
 
-import prisma from "@/lib/prisma"
-import type { Todo } from "@/generated/prisma"
+import { v4 as uuidv4 } from "uuid"
+import { Todo } from "@/lib/types"
 
-export type GetAllTodosResponse =
+type GetAllTodosResponse =
   | {
       todos: Todo[]
       error: null
@@ -13,14 +13,30 @@ export type GetAllTodosResponse =
       error: string
     }
 
+const TODOS: Todo[] = [
+  {
+    id: uuidv4(),
+    title: "Complete FOCI Solutions project",
+    description: "Finish developing the Todo app requested by FOCI Solutions",
+    dueDate: new Date(Date.now() + 2 * 24 * 60 * 60 * 1000), // 2 days from now
+    completedAt: new Date(),
+    createdAt: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000), // 5 days ago
+    updatedAt: new Date(Date.now() - 2 * 24 * 60 * 60 * 1000), // 2 days ago
+  },
+  {
+    id: uuidv4(),
+    title: "Deploy the app to Vercel",
+    description: "Make sure to successfully deploy the app to Vercel",
+    dueDate: new Date(Date.now() + 3 * 24 * 60 * 60 * 1000), // 3 days from now
+    completedAt: null,
+    createdAt: new Date(Date.now() - 5 * 24 * 60 * 60 * 1000), // 5 days ago
+    updatedAt: new Date(Date.now() - 1 * 24 * 60 * 60 * 1000), // 1 day ago
+  },
+]
+
 export default async function getAllTodos(): Promise<GetAllTodosResponse> {
   try {
-    const todos = await prisma.todo.findMany({
-      orderBy: {
-        createdAt: "desc",
-      },
-    })
-    return { todos, error: null }
+    return { todos: TODOS, error: null }
   } catch (error) {
     console.error("Failed to get all todos:", error)
     return { todos: null, error: "Failed to fetch todos" }
