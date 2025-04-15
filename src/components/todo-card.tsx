a5e6b06f4603df2af8a52014c6ce9a5dada88da0
Salim Dellali <dellali.salim@gmail.com>
2025-04-15 15:16:24 -0400

    refactor todo-card.tsx by enhancing status handling and improve responsiveness

--- src/components/todo-card.tsx
+++ src/components/todo-card.tsx
@@ -1,6 +1,11 @@
 "use client"
 
-import { formatDistanceToNow } from "date-fns"
+import {
+  formatDistanceToNow,
+  formatRelative,
+  differenceInDays,
+  differenceInSeconds,
+} from "date-fns"
 import { Todo, TodoInput } from "@/db/schema"
 import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card"
 import { Checkbox } from "@/components/ui/checkbox"
@@ -27,6 +32,7 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { TodoFormDialog } from "@/components/todo-form-dialog"
+import { cn } from "@/lib/utils"
 
 type Props = {
   todo: Todo
@@ -78,21 +84,83 @@ export function TodoCard({
   onUpdate,
   onToggleCompletedAt,
 }: Readonly<Props>) {
-  const getDistanceToNow = (date: Date) => {
+  const getDistanceToNow = (date: Date): string => {
     return formatDistanceToNow(date, { addSuffix: true })
   }
 
+  const formatCompletedAt = (date: Date): string => {
+    return formatRelative(date, new Date())
+  }
+
+  enum TodoStatusEnum {
+    COMPLETED = "Completed",
+    IN_PROGRESS = "In Progress",
+    DUE_SOON = "Due Soon",
+    OVERDUE = "Overdue",
+  }
+
+  const getTodoStatus = (todo: Todo): TodoStatusEnum => {
+    if (todo.completedAt) {
+      return TodoStatusEnum.COMPLETED
+    }
+
+    const daysToDueDate = differenceInDays(todo.dueDate, new Date())
+    if (daysToDueDate < 0) {
+      return TodoStatusEnum.OVERDUE
+    }
+    if (daysToDueDate <= 1) {
+      return TodoStatusEnum.DUE_SOON
+    }
+
+    return TodoStatusEnum.IN_PROGRESS
+  }
+
+  const getStatusColor = (status: TodoStatusEnum) => {
+    switch (status) {
+      case TodoStatusEnum.COMPLETED:
+        return "bg-emerald-400"
+      case TodoStatusEnum.OVERDUE:
+        return "bg-red-400"
+      case TodoStatusEnum.DUE_SOON:
+        return "bg-orange-400"
+      default:
+        return "bg-primary"
+    }
+  }
+
+  const todoStatus = getTodoStatus(todo)
+
   return (
-    <Card className="group hover:shadow-lg transition-all duration-300">
-      <CardHeader className="flex justify-between gap-3 ">
+    <Card
+      className={cn(
+        "group relative overflow-hidden hover:shadow-lg transition-all duration-300",
+        todo.completedAt && "bg-muted text-muted-foreground"
+      )}
+    >
+      {/* Priority indicator line */}
+      <div
+        className={cn(
+          "absolute left-0 top-0 bottom-0 w-1",
+          getStatusColor(todoStatus)
+        )}
+      />
+
+      <CardHeader className="flex justify-between gap-3">
         {/* checkbox and title */}
         <div className="flex gap-3 items-center">
           <Checkbox
-            className="rounded-full border-2"
+            className="size-6 rounded-full border-2 data-[state=checked]:bg-emerald-400 data-[state=checked]:text-white"
             checked={!!todo.completedAt}
             onCheckedChange={() => onToggleCompletedAt(todo)}
           />
-          <h3 className="font-medium text-lg">{todo.title}</h3>
+          <h3
+            className={cn(
+              "font-medium text-lg",
+              todo.completedAt && "line-through"
+            )}
+          >
+            {todo.title}
+          </h3>
         </div>
 
         {/* actions */}
@@ -112,49 +180,59 @@ export function TodoCard({
       </CardHeader>
 
       <CardContent className="space-y-2">
-        {/* status*/}
-        <Badge>In progress</Badge>
+        {/* status */}
+        <Badge className={getStatusColor(todoStatus)}>{todoStatus}</Badge>
 
         {/* description */}
         <p>{todo.description}</p>
       </CardContent>
 
       <CardFooter>
-        <div className="flex flex-col w-full pt-6 border-t text-xs text-muted-foreground">
-          <div className="flex flex-col sm:flex-row sm:justify-between">
+        <div className="w-full pt-6 border-t">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-1 text-xs text-muted-foreground">
             {/* completed metadata */}
             <div className="flex items-center">
               {todo.completedAt ? (
                 <>
-                  <CircleCheckBig className="size-3 mr-1 text-emerald-500" />
-                  <span className="text-emerald-600 dark:text-emerald-600">
-                    Completed {getDistanceToNow(todo.completedAt)}
+                  <CircleCheckBig className="size-3 mr-1 shrink-0" />
+                  <span className="truncate">
+                    Completed {formatCompletedAt(todo.completedAt)}
                   </span>
                 </>
               ) : (
                 <>
-                  <CircleX className="size-3 mr-1" />
-                  <span>Not Completed</span>
+                  <CircleX className="size-3 mr-1 shrink-0" />
+                  <span className="truncate">
+                    {differenceInSeconds(todo.dueDate, new Date()) > 0
+                      ? "Not yet completed"
+                      : "Not completed"}
+                  </span>
                 </>
               )}
             </div>
 
             {/* due date metadata */}
             <div className="flex items-center">
-              <Calendar className="size-3 mr-1" />
-              <span>Due {getDistanceToNow(todo.dueDate)}</span>
+              <Calendar className="size-3 mr-1 shrink-0" />
+              <span className="truncate">
+                Due {getDistanceToNow(todo.dueDate)}
+              </span>
             </div>
 
             {/* updated metadata */}
             <div className="flex items-center">
-              <RefreshCw className="size-3 mr-1" />
-              <span>Updated {getDistanceToNow(todo.updatedAt)}</span>
+              <RefreshCw className="size-3 mr-1 shrink-0" />
+              <span className="truncate">
+                Updated {getDistanceToNow(todo.updatedAt)}
+              </span>
             </div>
 
             {/* created metadata */}
             <div className="flex items-center">
-              <Clock className="size-3 mr-1" />
-              <span>Created {getDistanceToNow(todo.createdAt)}</span>
+              <Clock className="size-3 mr-1 shrink-0" />
+              <span className="truncate">
+                Created {getDistanceToNow(todo.createdAt)}
+              </span>
             </div>
           </div>
         </div>
